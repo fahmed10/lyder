c50dedf6725860ebe6481647117d26f4de89ccee
fahmed10 <fahmed10@ggc.edu>
2025-03-30 14:59:35 -0400

    Add TypeScript support for JSX

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
-import { createElement, createRoot, Fragment, useState } from "./lyder";
+import { createElement, createRoot, Fragment, LyderElement, LyderKey, LyderRenderable, useState } from "./lyder";
 
 export default { createRoot, createElement, useState, Fragment };
 export { createRoot, createElement, useState, Fragment } from "./lyder";
 export type { LyderElement, LyderKey, LyderRenderable } from "./lyder";
+
+declare global {
+    namespace Lyder {
+        namespace JSX {
+            export interface IntrinsicAttributes {
+                key?: LyderKey;
+                [key: string]: any;
+            }
+
+            export interface IntrinsicElements {
+                [key: string]: any;
+            }
+
+            export type Element = LyderRenderable;
+            export type ElementClass = LyderElement;
+
+            export interface ElementAttributesProperty {
+                props: any;
+            }
+
+            export interface ElementChildrenAttribute {
+                children: any;
+            }
+        }
+    }
+}
